bfa03c44f7d580285bb9177933a341ac893e79cd
anananna0v0 <joanna19990422@gmail.com>
2025-03-25 09:16:12 +0100

    chore: install ethers and dotenv; check .env path

--- scripts/assignment_1/quiz_template.js
+++ scripts/assignment_1/quiz_template.js
@@ -13,70 +13,71 @@ const ethers = require("ethers");
 
 // Adjust path to your .env file.
 const pathToDotEnv = path.join(__dirname, "..", "..", ".env");
-// console.log(pathToDotEnv);
+
 require("dotenv").config({ path: pathToDotEnv });
+console.log(pathToDotEnv);
 
-const { getUserAnswer, extractQuestion } =
-    require(path.join(__dirname, "quiz_helper.js"));
+// const { getUserAnswer, extractQuestion } =
+//     require(path.join(__dirname, "quiz_helper.js"));
 
-// Create Signer and Contract.
-//////////////////////////////
+// // Create Signer and Contract.
+// //////////////////////////////
 
-const providerKey = process.env.ALCHEMY_KEY;
-const sepoliaUrl = `${process.env.ALCHEMY_SEPOLIA_API_URL}${providerKey}`;
-// console.log(sepoliaUrl);
-const sepoliaProvider = new ethers.JsonRpcProvider(sepoliaUrl);
+// const providerKey = process.env.ALCHEMY_KEY;
+// const sepoliaUrl = `${process.env.ALCHEMY_SEPOLIA_API_URL}${providerKey}`;
+// // console.log(sepoliaUrl);
+// const sepoliaProvider = new ethers.JsonRpcProvider(sepoliaUrl);
 
-const signer = new ethers.Wallet(
-    process.env.METAMASK_1_PRIVATE_KEY,
-    sepoliaProvider
-);
+// const signer = new ethers.Wallet(
+//     process.env.METAMASK_1_PRIVATE_KEY,
+//     sepoliaProvider
+// );
 
-const quizABI = require(path.join(__dirname, "quiz_abi"));
+// const quizABI = require(path.join(__dirname, "quiz_abi"));
 
 // The address of the Quiz contract.
-const contractAddress = "0x01FaE6a3E15b8cf2cb89C259b2d6e5bf7cf94782";
+// const contractAddress = "0x01FaE6a3E15b8cf2cb89C259b2d6e5bf7cf94782";
 
-const quizContract = new ethers.Contract(contractAddress, quizABI, signer);
+// const quizContract = new ethers.Contract(contractAddress, quizABI, signer);
 
-async function main() {
+// async function main() {
 
-    // A. Ask question and get a transaction receipt.
-    // Hint: method `askQuestion()`
+//     // A. Ask question and get a transaction receipt.
+//     // Hint: method `askQuestion()`
 
-    // Your code here.
+//     // Your code here.
 
-    // From the transaction receipt we can extract useful information, such as
-    // as the question's text and id that were stored in the logs
-    // (we will understand logs in detail later in the course).
-    const { text, id } = extractQuestion(quizContract, receipt);
+//     // From the transaction receipt we can extract useful information, such as
+//     // as the question's text and id that were stored in the logs
+//     // (we will understand logs in detail later in the course).
+//     const { text, id } = extractQuestion(quizContract, receipt);
 
-    if (!text) {
-        console.log('An error occurred, please try again.');
-        console.log('More info here: https://sepolia.etherscan.io/tx/' +
-            receipt.hash);
-        return;
-    }
+//     if (!text) {
+//         console.log('An error occurred, please try again.');
+//         console.log('More info here: https://sepolia.etherscan.io/tx/' +
+//             receipt.hash);
+//         return;
+//     }
 
-    // Now YOU answer the question!
-    // Capture user input from the terminal.
-    const userAnswer = await getUserAnswer();
+//     // Now YOU answer the question!
+//     // Capture user input from the terminal.
+//     const userAnswer = await getUserAnswer();
 
-    // B. Send the answer to the smart contract.
-    // Hint: method `answerQuestion`.
+//     // B. Send the answer to the smart contract.
+//     // Hint: method `answerQuestion`.
 
-    // Your code here.
+//     // Your code here.
 
-    // C. Optional. Verify that the answer is correctly stored.
-    // Hint: method `getAnswer(questionId)`
+//     // C. Optional. Verify that the answer is correctly stored.
+//     // Hint: method `getAnswer(questionId)`
 
-    // Your code here.
-}
+//     // Your code here.
+// }
 
 
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+// main()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//         console.error(error);
+//         process.exit(1);
+//     });
