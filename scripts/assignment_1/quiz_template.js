a691498c42df247e5661d66f0d6483dddbb3827f
anananna0v0 <joanna19990422@gmail.com>
2025-03-25 09:20:12 +0100

    fix: correct quiz_abi path in quiz_template.js

--- scripts/assignment_1/quiz_template.js
+++ scripts/assignment_1/quiz_template.js
@@ -15,25 +15,25 @@ const ethers = require("ethers");
 const pathToDotEnv = path.join(__dirname, "..", "..", ".env");
 
 require("dotenv").config({ path: pathToDotEnv });
-console.log(pathToDotEnv);
+// console.log(pathToDotEnv);
 
-// const { getUserAnswer, extractQuestion } =
-//     require(path.join(__dirname, "quiz_helper.js"));
+const { getUserAnswer, extractQuestion } =
+    require(path.join(__dirname, "quiz_helper.js"));
 
-// // Create Signer and Contract.
-// //////////////////////////////
+// Create Signer and Contract.
+//////////////////////////////
 
-// const providerKey = process.env.ALCHEMY_KEY;
-// const sepoliaUrl = `${process.env.ALCHEMY_SEPOLIA_API_URL}${providerKey}`;
-// // console.log(sepoliaUrl);
-// const sepoliaProvider = new ethers.JsonRpcProvider(sepoliaUrl);
+const providerKey = process.env.ALCHEMY_KEY;
+const sepoliaUrl = `${process.env.ALCHEMY_SEPOLIA_API_URL}${providerKey}`;
+// console.log(sepoliaUrl);
+const sepoliaProvider = new ethers.JsonRpcProvider(sepoliaUrl);
 
-// const signer = new ethers.Wallet(
-//     process.env.METAMASK_1_PRIVATE_KEY,
-//     sepoliaProvider
-// );
+const signer = new ethers.Wallet(
+    process.env.METAMASK_1_PRIVATE_KEY,
+    sepoliaProvider
+);
 
-// const quizABI = require(path.join(__dirname, "quiz_abi"));
+const quizABI = require(path.join(__dirname, "quiz_abi.json"));
 
 // The address of the Quiz contract.
 // const contractAddress = "0x01FaE6a3E15b8cf2cb89C259b2d6e5bf7cf94782";
